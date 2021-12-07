e59267496719fdf9f1a45e11068e2804902f83bd
Gurpreet1490 <g.preet08092014@gmail.com>
2021-12-07 15:21:57 +0000

    first attempt (ex01)

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,32 +1,39 @@
 package com.techreturners.exercise001;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        word = "Hello World";
-
-        return "word.toUpperCase()";
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        int fullName = firstName.charAt(0) + lastName.charAt(0);
+        return "fullName";
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double addingVat = originalPrice + (originalPrice * vatRate) / 100;
+        addingVat = Math.round((addingVat * 100)/100);
+        return addingVat;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        if (sentence == null){
+            return null;
+        }
+        StringBuilder output = new StringBuilder(sentence).reverse();
+        return output.toString();
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int countLinux = 0;
+        for (User myOs : users){
+            if (myOs.getType().equals("Linux")){
+                countLinux += 1;
+            }
+        }
+        return countLinux;
     }
 }
